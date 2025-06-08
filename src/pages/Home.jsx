cdb5d5c45f562283e3d117eed18e434e19834929
deepdeb <debnath.pradeep619@gmail.com>
2025-06-08 16:49:59 +0530

    added simple carousel with 3 images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,34 @@
 import { Categories } from '../assets/mockData'
-import HeroImage from '../assets/Images/hero-page.png'
+import HeroImage1 from '../assets/Images/shopping-woman.png'
+import HeroImage2 from '../assets/Images/shopping-girl.png'
+import HeroImage3 from '../assets/Images/shopping-man.png'
 import InfoSection from '../components/InfoSection'
 import { useSelector } from 'react-redux'
 import ProductCard from '../components/ProductCard'
 import Shop from './Shop'
+import { useEffect, useState } from 'react'
+
+const images = [HeroImage1, HeroImage2, HeroImage3];
 
 const Home = () => {
   const { products } = useSelector(state => state.product)
+
+  const [index, setIndex] = useState(0);
+
+  // Auto-play every 5 seconds
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prev) => (prev + 1) % images.length);
+    }, 3000);
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <div className="bg-gray-50">
 
       <div className="px-30 my-20">
         <div className="w-full relative **aspect-video** rounded-lg overflow-hidden shadow-lg mb-12">
-          <img className="h-full w-full object-cover" src={HeroImage} alt="Hero" />
+          <img className="h-full w-full object-cover" src={images[index]} alt="Hero" />
           <div className="absolute inset-0 bg-black/10"></div>
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center max-w-lg">
             <h2 className="text-4xl font-extrabold leading-snug drop-shadow-lg">WELCOME TO MY E-COMM SHOP</h2>
@@ -21,6 +36,18 @@ const Home = () => {
             <button className="mt-5 bg-red-600 hover:bg-red-700 transition-all duration-300 text-white px-6 py-2 rounded shadow hover:scale-105">
               SHOP NOW
             </button>
+          </div>
+
+          {/* Dots */}
+          <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex space-x-2">
+            {images.map((_, i) => (
+              <button
+                key={i}
+                className={`w-3 h-3 rounded-full ${index === i ? "bg-white" : "bg-white/50"
+                  }`}
+                onClick={() => setIndex(i)}
+              />
+            ))}
           </div>
         </div>
       </div>
